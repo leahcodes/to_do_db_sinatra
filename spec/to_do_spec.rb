9adc58dc1b09e3ab4727ced606411adafd6d05ba
Andrew DAng & Andrew Brennwald <student@epicodus.com>
2015-08-11 09:48:38 -0700

    Add passing code and specs for saving tasks to the array of all tasks and recalling them.

--- spec/to_do_spec.rb
+++ spec/to_do_spec.rb
@@ -4,14 +4,24 @@ require('to_do')
 describe(Task) do
   describe('#description') do
     it('lets you give it a description') do
-      test_task = Task.new('scrub the zebra')
-      expect(test_task.description()).to(eq("scrub the zebra"))
+      test_task = Task.new("acquire benjamins")
+      expect(test_task.description()).to(eq("acquire benjamins"))
     end
   end
 
   describe(".all") do
     it("initializes an empty array to hold class variables later") do
       expect(Task.all()).to(eq([]))
+    end
+  end
+
+  describe("#save") do
+    it('adds two tasks to the array of saved tasks') do
+      test_task = Task.new('do laundry naked')
+      test_task.save()
+      test_task_two = Task.new('pick up dog shit')
+      test_task_two.save()
+      expect(Task.all()).to(eq([test_task, test_task_two]))
     end
   end
 end
